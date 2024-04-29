fa5b2ccc4f66f7a8dc4c8cbb611d094ae4082455
toursingapore <158420206+toursingapore@users.noreply.github.com>
2024-04-29 11:56:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,53 @@
-import streamlit as st
+website = st.text_input("Enter your website to crawl")
+button = st.button("SUBMIT", type="primary" , key="1")
+if button:
+    st.write(f"your website is {website}")  
+    with st.container():
+        with st.spinner('Wait for it...'):
+            time.sleep(5)
 
-"""
-## hello Web scraping on Streamlit Cloud with Selenium
+            #@st.cache_resource  #Phải chuyển comment cái này để nó ko nhớ cache và crawl nhiều urls được
+            def get_driver():
+                return webdriver.Chrome(
+                    service=Service(
+                        ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+                    ),
+                    options=options,
+                )
 
-[![Source](https://img.shields.io/badge/View-Source-<COLOR>.svg)](https://github.com/snehankekre/streamlit-selenium-chrome/)
+            options = Options()
+            options.add_argument("--disable-gpu")
+            options.add_argument("--headless=new")
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+            options.add_argument("--enable-javascript")
+            options.add_argument("user-agent=Mozilla/5.0 (Linux; Android 13; SAMSUNG SM-G988B) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/20.0 Chrome/106.0.5249.126 Mobile Safari/537.36")
+            #proxy = '23.23.23.23:3128'
+            #options.add_argument('--proxy-server='+proxy) #use proxy with --proxy-server=23.23.23.23:3128
+            #options.add_argument('--proxy-server=socks5://'+proxy) #use socks5 with --proxy-server=socks5://23.23.23.23:3128
 
-This is a minimal, reproducible example of how to scrape the web with Selenium and Chrome on Streamlit's Community Cloud.
+            driver = get_driver()
+            driver.get(website) #driver.get("https://vnexpress.net")
 
-Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
-"""
+            def wait_for_page_load(driver): 
+                return driver.execute_script('return document.readyState') == 'complete'             
+            
+            Page_Loaded = wait_for_page_load(driver)
+            if Page_Loaded:
+                st.write(f"Page Loaded: {Page_Loaded}")
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
+                html = driver.page_source
+                #st.code(html) #show code html để user nhìn thấy
+                st.markdown(html, unsafe_allow_html=True) #load html and render it in streamlit page
+                
+                #Đưa vào BeautifulSoup cho dễ scrape elements
+                soup = BeautifulSoup(html)
+                for tag in soup.find_all('title'):
+                    st.write(tag.text)
+                for tag_body in soup.find_all('body'):
+                    st.write(tag_body.text)
 
-    #@st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-            ),
-            options=options,
-        )
-
-    options = Options()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
-
-    driver = get_driver()
-    driver.get("https://whoer.net/")
-
-    st.code(driver.page_source)
+                # Quit the driver
+                driver.close()
+                driver.quit()
+        st.success('Done!')
