7dc9758106912622f3f0fc31c5a9eb6da59c6421
toursingapore <158420206+toursingapore@users.noreply.github.com>
2024-04-29 02:54:28 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,6 @@ with st.echo():
     options.add_argument("--headless")
 
     driver = get_driver()
-    driver.get("http://example.com")
+    driver.get("https://vnexpress.net")
 
     st.code(driver.page_source)
